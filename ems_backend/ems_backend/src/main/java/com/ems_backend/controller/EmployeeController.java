e7765b9e923ffb0748d818c1e06ae4a28133f5b5
RohitPatil <rohit.patil@atyeti.com>
2025-09-01 12:30:32 +0530

    1st CICD

--- ems_backend/ems_backend/src/main/java/com/ems_backend/controller/EmployeeController.java
+++ ems_backend/ems_backend/src/main/java/com/ems_backend/controller/EmployeeController.java
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://benevolent-choux-5a52fc.netlify.app/")
-
+@CrossOrigin(origins = "https://luxury-platypus-a65c4f.netlify.app/")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
